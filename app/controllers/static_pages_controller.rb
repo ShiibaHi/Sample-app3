fc73049c2fe0ee5488f950a45cedce7c826f5da7
ShiibaHi <130768370+ShiibaHi@users.noreply.github.com>
2023-06-10 04:39:10 +0000

    Exec:rails generate controller StaticPages home help

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+    # render ....
+    # => app/views/static_pages/home.html.erb
+  end
+  
+  # app/voews/コントロール名/アクション名.html.erb
+  def help
+  end
+end
